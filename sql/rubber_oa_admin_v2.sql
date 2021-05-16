39ef21d90b829a4f58d1aa2c6b1960902b4877ef
luffyu <919132691@qq.com>
2021-05-16 17:23:00 +0800

    修改端口

--- sql/rubber_oa_admin_v2.sql
+++ sql/rubber_oa_admin_v2.sql
@@ -8,3 +8,6 @@ alter table room_contrast_config add float_type varchar(50) not null default 'ad
 alter table hotel_room_sync_exec_water add origin_price  double not null default 0 COMMENT '数据源的价格';
 alter table hotel_room_sync_exec_water add float_price double not null default 0 COMMENT '浮动的价格';
 alter table hotel_room_sync_exec_water add float_type varchar(50) not null default 'add' COMMENT 'add表示新增 reduce表示减少';
+
+
+alter table hotel_room_sync_exec_water add index index_sync_time (sync_time) ;
